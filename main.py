c43d186b1123aa5e478591b86fcc7ec246325480
Gabriel Seabra <gmatos@hotmail.com.br>
2026-05-18 18:25:05 -0300

    feat: limpeza e tratamento de dados

--- main.py
+++ main.py
@@ -2,14 +2,17 @@ import pandas as pd
 
 dados = pd.read_csv("dados/clientes.csv")
 
-print("INFORMACOES DO DATASET:")
-print(dados.info())
+print("DATASET ORIGINAL:")
+print(dados)
 
-print("\nCOLUNAS:")
-print(dados.columns)
+print("\nVALORES NULOS:")
+print(dados.isnull().sum())
 
-print("\nDIMENSOES:")
-print(dados.shape)
+dados["idade"] = dados["idade"].fillna(dados["idade"].mean())
 
-print("\nESTATISTICAS:")
-print(dados.describe())
+dados["salario"] = dados["salario"].fillna(dados["salario"].mean())
+
+dados = dados.drop_duplicates()
+
+print("\nDATASET LIMPO:")
+print(dados)
