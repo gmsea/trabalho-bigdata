6704cf73ebaa9cf1c5a8b05afe85299bf3d9dd5f
Gabriel Seabra <gmatos@hotmail.com.br>
2026-05-18 17:49:39 -0300

    feat: configuração de exibição e visualização de linhas

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import pandas as pd
 
+pd.set_option("display.max_rows", 10)
+
 dados = pd.read_csv("dados/clientes.csv")
 
-subconjunto = dados[["nome", "cidade", "salario"]]
+print("PRIMEIRAS LINHAS:")
+print(dados.head())
 
-print(subconjunto)
+print("\nULTIMAS LINHAS:")
+print(dados.tail())
