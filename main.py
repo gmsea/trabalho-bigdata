b3f3b49a2396ec0ce08d6cf3fc7d5b78af2395e8
Gabriel Seabra <gmatos@hotmail.com.br>
2026-05-18 18:10:47 -0300

    feat: análise estrutural do dataset

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import pandas as pd
 
-pd.set_option("display.max_rows", 10)
-
 dados = pd.read_csv("dados/clientes.csv")
 
-print("PRIMEIRAS LINHAS:")
-print(dados.head())
+print("INFORMACOES DO DATASET:")
+print(dados.info())
 
-print("\nULTIMAS LINHAS:")
-print(dados.tail())
+print("\nCOLUNAS:")
+print(dados.columns)
+
+print("\nDIMENSOES:")
+print(dados.shape)
+
+print("\nESTATISTICAS:")
+print(dados.describe())
