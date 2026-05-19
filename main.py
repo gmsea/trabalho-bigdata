63e7f4df307fc3a37ec8f82086e2639b1540416a
Gabriel Seabra <gmatos@hotmail.com.br>
2026-05-18 22:55:50 -0300

    feat: finalização do tratamento e limpeza dos dados

--- main.py
+++ main.py
@@ -8,14 +8,28 @@ dados = pd.read_csv(
 
 copia_dados = dados.copy()
 
+copia_dados["Calories"] = copia_dados["Calories"].fillna(0)
+
+copia_dados["Date"] = copia_dados["Date"].fillna("1900/01/01")
+
+print("ANTES DA CONVERSAO:")
+print(copia_dados.to_string())
+
 copia_dados["Date"] = copia_dados["Date"].replace(
-    20201226,
-    "2020/12/26"
+    "1900/01/01",
+    pd.NA
+)
+
+copia_dados["Date"] = copia_dados["Date"].replace(
+    "20201226",
+    pd.to_datetime("2020/12/26")
 )
 
 copia_dados["Date"] = pd.to_datetime(
-    copia_dados["Date"],
-    errors="coerce"
+    copia_dados["Date"]
 )
 
-print(copia_dados)
+copia_dados = copia_dados.dropna()
+
+print("\nDEPOIS DA CONVERSAO:")
+print(copia_dados.to_string())
